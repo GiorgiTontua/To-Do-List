8d75585e0026f71577d0c7dcd80fee460fe2c29e
GiorgiTontua <giorgi.tontua@gmail.com>
2024-05-18 19:57:07 +0400

    First commit.

--- src/Main.java
+++ src/Main.java
@@ -26,7 +26,7 @@ import javafx.stage.Stage;
 
 import java.util.HashMap;
 
-//Giorgi Tontua
+//Giorgi Tontuaa
 public class Main extends Application {
 
     public static void main(String[] args) {
